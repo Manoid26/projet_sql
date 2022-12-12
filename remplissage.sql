66a9fa2434dc26c6de8c649da73de1b09e260c68
manoid26 <salmanghrs@gmail.com>
2022-12-12 12:52:01 +0100

    premier remplissage test

---
+++ remplissage.sql
@@ -0,0 +1,205 @@
+/* PAYSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS*/
+
+Insert Into pays(nom, langue_origine)
+Values('Etas-Unis','Anglais');
+
+Insert Into pays(nom, langue_origine)
+Values('Chine','Mandarin');
+
+Insert Into pays(nom, langue_origine)
+Values('Japon','Japonais');
+
+Insert Into pays(nom, langue_origine)
+Values('Allemagne','Allemand');
+
+Insert Into pays(nom, langue_origine)
+Values('France','Francais');
+Insert Into pays(nom, langue_origine)
+Values('Inde','Hindi');
+
+Insert Into pays(nom, langue_origine)
+Values('Italie','Italien');
+
+Insert Into pays(nom, langue_origine)
+Values('Bresil','Portugais');
+
+Insert Into pays(nom, langue_origine)
+Values('Canada','Anglais');
+
+Insert Into pays(nom, langue_origine)
+Values('Russie','Russe');
+
+Insert Into pays(nom, langue_origine)
+Values('Coree','Coreen');
+
+Insert Into pays(nom, langue_origine)
+Values('Australie','Anglais');
+
+Insert Into pays(nom, langue_origine)
+Values('Espagne','Espagnol');
+
+Insert Into pays(nom, langue_origine)
+Values('Mexique','Espagnol');
+
+Insert Into pays(nom, langue_origine)
+Values('Indonesie','Indonesien');
+
+Insert Into pays(nom, langue_origine)
+Values('Pays-bas','Neerlandaise');
+
+Insert Into pays(nom, langue_origine)
+Values('Arabie Saoudite','Arabe');
+
+Insert Into pays(nom, langue_origine)
+Values('Turquie','Turc');
+
+Insert Into pays(nom, langue_origine)
+Values('Pologne','Polonais');
+
+Insert Into pays(nom, langue_origine)
+Values('Suede','Suedois');
+
+Insert Into pays(nom, langue_origine)
+Values('Belgique','Francais');
+
+Insert Into pays(nom, langue_origine)
+Values('Argentine','Espagnol');
+
+Insert Into pays(nom, langue_origine)
+Values('Thailande','Thai');
+
+Insert Into pays(nom, langue_origine)
+Values('Venezuela','Espagnol');
+
+Insert Into pays(nom, langue_origine)
+Values('Autriche','Allemand');
+
+Insert Into pays(nom, langue_origine)
+Values('Iran','Iranien');
+
+Insert Into pays(nom, langue_origine)
+Values('Norvege','Norvegien');
+
+Insert Into pays(nom, langue_origine)
+Values('Nigeria','Anglais');
+
+Insert Into pays(nom, langue_origine)
+Values('Irlande','Irlandais');
+
+Insert Into pays(nom, langue_origine)
+Values('Israel','Hebreu');
+
+Insert Into pays(nom, langue_origine)
+Values('Afrique_du_sud','Zoulou');
+
+Insert Into pays(nom, langue_origine)
+Values('Singapour','Tamoul');
+
+Insert Into pays(nom, langue_origine)
+Values('Danemark','Danois');
+
+Insert Into pays(nom, langue_origine)
+Values('Philipines','Anglais');
+
+Insert Into pays(nom, langue_origine)
+Values('','');
+
+Insert Into pays(nom, langue_origine)
+Values('Colombie','Espagnol');
+
+Insert Into pays(nom, langue_origine)
+Values('Pakistan','Ourdou');
+
+Insert Into pays(nom, langue_origine)
+Values('Chili','Espagnol');
+
+Insert Into pays(nom, langue_origine)
+Values('Finlande','Finnois');
+
+Insert Into pays(nom, langue_origine)
+Values('Bangladesh','Bengali');
+
+Insert Into pays(nom, langue_origine)
+Values('Egypte','Arabe');
+
+Insert Into pays(nom, langue_origine)
+Values('Vietnam','Vietnamien');
+
+Insert Into pays(nom, langue_origine)
+Values('Roumanie','Roumain');
+
+Insert Into pays(nom, langue_origine)
+Values('Portugal','Portugais');
+
+Insert Into pays(nom, langue_origine)
+Values('Irak','Arabe');
+
+Insert Into pays(nom, langue_origine)
+Values('Perou','Espagnol');
+
+Insert Into pays(nom, langue_origine)
+Values('Grece','Grec');
+
+Insert Into pays(nom, langue_origine)
+Values('Nouvelle-zelande','Maori');
+
+Insert Into pays(nom, langue_origine)
+Values('Qatar','Arabe');
+
+Insert Into pays(nom, langue_origine)
+Values('Algerie','Arabe');
+
+Insert Into pays(nom, langue_origine)
+Values('Cuba','Espagnol');
+
+Insert Into pays(nom, langue_origine)
+Values('Cameroun','Francais');
+
+/*--------------Centre spacial-------------*/
+
+Insert Into centre_spacial(nom,id_pays,directeur)
+Values('NASA','1','Bill Nelson');
+
+Insert Into centre_spacial(nom,id_pays,directeur)
+Values('CNSA','2','');
+
+Insert Into centre_spacial(nom,id_pays,directeur)
+Values('UAESA','','');
+
+Insert Into centre_spacial(nom,id_pays,directeur)
+Values('CNES','','');
+
+Insert Into centre_spacial(nom,id_pays,directeur)
+Values('ROSCOSMOS','','');
+
+Insert Into centre_spacial(nom,id_pays,directeur)
+Values('JAXA','','');
+
+Insert Into centre_spacial(nom,id_pays,directeur)
+Values('ISRO','','');
+
+Insert Into centre_spacial(nom,id_pays,directeur)
+Values('ASI','','');
+
+Insert Into centre_spacial(nom,id_pays,directeur)
+Values('ASC','','');
+
+Insert Into centre_spacial(nom,id_pays,directeur)
+Values('UKSA','','');
+
+Insert Into centre_spacial(nom,id_pays,directeur)
+Values('ASAL','','');
+
+Insert Into centre_spacial(nom,id_pays,directeur)
+Values('ISA','','');
+
+Insert Into centre_spacial(nom,id_pays,directeur)
+Values('ISA','','');
+
+
+Insert Into centre_spacial(nom,id_pays,directeur)
+Values('CAMSPACE','','');
+
+
+Insert Into centre_spacial(nom,id_pays,directeur)
+Values('BDD','','');
